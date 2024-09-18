836ed4cb975fe2d4fcacd869b93f0b25f853bc18
Nick Maslov <kategorychnost@gmail.com>
2024-09-17 21:33:28 -0400

    created users controller and roouter and authentification middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import authentification from "./autentication";
+import users from "./users";
 
 const router = express.Router();
 
 export default (): express.Router => {
   authentification(router);
+  users(router);
 
   return router;
 };
